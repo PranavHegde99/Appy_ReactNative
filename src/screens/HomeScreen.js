75a993eb4950558396811833fb5dea8cb77c8dc4
Panu99 <hegdepanu4@gmail.com>
2020-12-24 18:23:24 +0530

    commit

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,7 +4,7 @@ import { Text, StyleSheet,View,Button, TouchableOpacity } from "react-native";
 const HomeScreen = (props) => {
   console.log(props.navigation);
   return ( 
-    <View>
+    <View style={styles.viewstyle}>
     <Text style={styles.text}>Welcome To First Web Pages</Text>
      <Text style={styles.text}>Sisri</Text>
     <Button onPress={() => props.navigation.navigate('Image')}
@@ -22,8 +22,14 @@ const HomeScreen = (props) => {
 };
 
 const styles = StyleSheet.create({
+  viewstyle : {
+     borderWidth:3,
+     borderColor:'green'
+  },
   text: {
-    fontSize: 30
+    fontSize: 30,
+    borderWidth:3,
+     borderColor:'blue'
   },
   Buttonstyle: {
     height:80,
